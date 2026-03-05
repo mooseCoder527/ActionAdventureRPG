2f0b30a55ce3305cf6a98cd76edd93d9bd84892b
mooseCoder527 <123958914+mooseCoder527@users.noreply.github.com>
2026-03-05 14:54:07 +0100

    V0.14.3
    Scene Transitions work!
    -will add scenetranstion animation but other than that all set

--- Levels/Scripts/level_transition.gd
+++ Levels/Scripts/level_transition.gd
@@ -27,7 +27,48 @@ enum SIDE { LEFT , RIGHT , TOP , BOTTOM }
 
 func _ready() -> void:
 	_update_area()
+	if Engine.is_editor_hint():
+		return
+	
+	monitoring = false
+	_place_player()
+	
+	await LevelManager.level_loaded
+	
+	monitoring = true
+	body_entered.connect( _player_entered )
+	
+	
 	pass 
+
+
+func _player_entered( _p : Node2D ) -> void:
+	LevelManager.load_new_level( level , target_transition_area , get_offset())
+	pass
+
+
+func _place_player() -> void:
+	if name != LevelManager.target_transition:
+		return
+	PlayerManager.set_player_position( global_position + LevelManager.position_offset )
+
+
+func get_offset() -> Vector2:
+	var offset : Vector2 = Vector2.ZERO
+	var player_position = PlayerManager.player.global_position
+	
+	if side == SIDE.LEFT or side == SIDE.RIGHT:
+		offset.y = player_position.y - global_position.y
+		offset.x = 16
+		if side == SIDE.LEFT:
+			offset.x *= -1
+	else:
+		offset.x = player_position.x - global_position.x
+		offset.y = 16
+		if side == SIDE.TOP:
+			offset.y *= -1
+	
+	return offset
 
 
 func _update_area() -> void:
