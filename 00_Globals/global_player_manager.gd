a93b4c01bbe80985902c63367409a20499fc6420
Mika <mikagroth555@gmail.com>
2026-03-24 19:23:07 +0100

    v15.2
    SAVE & LOAD

--- 00_Globals/global_player_manager.gd
+++ 00_Globals/global_player_manager.gd
@@ -21,6 +21,11 @@ func set_player_position( new_pos : Vector2 ) -> void:
 	player.global_position = new_pos
 	pass
 
+func set_health( hp : int , max_hp : int ) -> void:
+	player.max_hp = hp
+	player.hp = hp
+	player.update_hp(0)
+
 func set_as_parent( _p : Node2D ) -> void:
 	if player.get_parent():
 		player.get_parent().remove_child( player )
--- GUI/pause_menu/pause_menu.gd
+++ GUI/pause_menu/pause_menu.gd
@@ -7,4 +7,49 @@ var is_paused : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	hide_pause_menu()
+	button_save.pressed.connect(_on_save_pressed)
+	button_load.pressed.connect(_on_load_pressed)
 	pass # Replace with function body.
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		if !is_paused:
+			show_pause_menu()
+			pass
+		else:
+			hide_pause_menu()
+			pass
+		get_viewport().set_input_as_handled()
+
+func show_pause_menu() -> void:
+	get_tree().paused = true
+	visible = true
+	is_paused = true
+	button_save.grab_focus()
+	pass
+
+func hide_pause_menu() -> void:
+	get_tree().paused = false
+	visible = false
+	is_paused = false
+	pass
+
+func _on_save_pressed() -> void:
+	if !is_paused:
+		return
+	SaveManager.save_game()
+	hide_pause_menu()
+	pass
+
+
+
+func _on_load_pressed() -> void:
+	if !is_paused:
+		return
+	SaveManager.load_game()
+	
+	await LevelManager.level_load_started
+	
+	hide_pause_menu()
+	pass
